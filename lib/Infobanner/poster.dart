1ac405299194968e76ab719c32d2229e144130dd
R_Srinath <srinath1412001@gmail.com>
2020-08-10 17:47:22 +0530

    Added Bezier Curve

--- lib/Infobanner/poster.dart
+++ lib/Infobanner/poster.dart
@@ -7,16 +7,39 @@ class Poster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        constraints: BoxConstraints.expand(
-          height: 400,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.grey,
-        ),
-        child: Image.asset(
-          _bannerPath,
-          fit: BoxFit.cover,
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: ClipPath(
+          clipper: ClippingClass(),
+          child: Container(
+              constraints: BoxConstraints.expand(
+                height: 400,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+              ),
+              child: Image.asset(
+                _bannerPath,
+                fit: BoxFit.cover,
+              )),
         ));
   }
+}
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 100);
+    path.quadraticBezierTo(
+        size.width / 4, size.height, size.width / 2, size.height);
+    path.quadraticBezierTo((size.width - size.width / 4), size.height,
+        size.width, size.height - 100);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: new ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(0xFF, 0x14, 0x1D, 0x26)),
+      debugShowCheckedModeBanner: false,
       home: Detail(),
     );
   }
